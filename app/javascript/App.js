b39c10368c936c3d8b94472644c348eb70dbbb25
edi-sipka@gmail.com <edisipka@gmail.com>
2023-01-10 08:22:09 +0100

    Add route on App component

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
 import Greetings from './Greetings'
 
 function App() {
-  return <Greetings />
+  return (
+    <Routes>
+      <Route path="/" element={<Greetings />} />
+    </Routes>
+  );
 }
 
 export default App
